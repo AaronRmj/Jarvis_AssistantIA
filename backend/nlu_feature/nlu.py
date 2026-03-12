20271825e814b26a7debe3ce601418ef91e93998
AaronRmj <aaronrmjk@gmail.com>
2026-03-12 18:50:41 +0300

    Nettoyage des donnees,apres split, partie I

--- backend/nlu_feature/nlu.py
+++ backend/nlu_feature/nlu.py
@@ -1,14 +1,31 @@
 import pandas as pd
 import numpy as np
+import string
 
-#On lit le fichier du dataset 
-try: 
-    data = pd.read_csv('dataset.csv',encoding="utf-8")
-    print("Fichier chargé avec succès")
-    print(data)
+#Lecture du fichier (dataset) 
+def charger_dataset():
+    try: 
+        with open('dataset.csv', 'r') as dataset:
+            data = dataset.read()
+        print("Fichier chargé avec succès")
+        print(data)
 
-except FileNotFoundError: 
-    print("Erreur, fichier non trouvé")
+    except FileNotFoundError: 
+        print("Erreur, fichier non trouvé")
 
-except Exception as e:
-    print("Une erreur est survenue")   
+    except Exception as e:
+        print("Une erreur est survenue")   
+
+charger_dataset()
+
+#Preprocessing, data cleaning
+
+def nettoyer_phrase():  #lowercase puis supprimer ponctuation, et enfin split la phrase
+    ponctuation = string.punctuation
+    phrase = str(input("Entrez une commande:\n")).lower()
+    for symbole in ponctuation:
+        phrase = phrase.replace(symbole,"")
+    resultat = phrase.split()
+    print(resultat)
+    
+nettoyer_phrase()
